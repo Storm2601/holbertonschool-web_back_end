0b049a57c4b84ab9450f6b2e2b8dce46674031a2
root <root@DESKTOP-B4F03QQ.localdomain>
2024-11-28 12:23:39 +0100

    Import the class Currency from 3-currency.js
    Implement a class named Pricing:
    
        Constructor attributes:
            amount (Number)
            currency (Currency)
        Each attribute must be stored in an “underscore” attribute version (ex: name is stored in _name)
        Implement a getter and setter for each attribute.
        Implement a method named displayFullPrice that returns the attributes in the following format amount currency_name (currency_code).
        Implement a static method named convertPrice. It should accept two arguments: amount (Number), conversionRate (Number). The function should return the amount multiplied by the conversion rate.

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,41 @@
+import Currency from "./3-currency";
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(newAmount) {
+    if (typeof newAmount !== "number") {
+      throw new TypeError("The amount must be a number");
+    }
+    this._amount = newAmount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(newCurrency) {
+    if (!(newCurrency instanceof Currency)) {
+      throw new TypeError("The currency must be an instance");
+    }
+    this._currency = newCurrency;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount !== "number" || typeof conversionRate !== "number") {
+      throw new TypeError("Amount and conversion rate must be numbers");
+    }
+    return amount * conversionRate;
+  }
+}
